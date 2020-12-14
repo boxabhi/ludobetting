e7400947884ebcee8fade9d7ecf5a0e292ab09d8
boxabhi <abhijeetg40@gmail.com>
2020-12-14 22:11:07 +0530

    -

--- game/urls.py
+++ game/urls.py
@@ -4,6 +4,5 @@ from .views import *
 
 urlpatterns = [
 
-    
-    
+ 
 ]
--- game/views.py
+++ game/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+def game_playing(request):
+    return render(request, 'game/game_playing.html')
