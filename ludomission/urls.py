98def5c4c4e19346239495b014b0a0a6519b340e
RKTBHU <tiwarirakesh360@gmail.com>
2020-12-16 21:48:09 +0530

    admin tamplets progress second commit

--- ludomission/urls.py
+++ ludomission/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('' , include('home.urls')),
     path('transaction/' , include('transaction.urls')),
     path('accounts/' , include('accounts.urls')),
-    path('adminpanel/' , include('adminpanel.urls')),
+    path('paneladmin/' , include('adminpanel.urls')),
     path('game/' , include('game.urls')),
     path('admin/', admin.site.urls),
 ]
