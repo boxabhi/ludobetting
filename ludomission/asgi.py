1d97240e4f308fec6a449cffe02204a631f96c23
boxabhi <abhijeetg40@gmail.com>
2020-12-19 17:31:21 +0530

    -

--- ludomission/asgi.py
+++ ludomission/asgi.py
@@ -15,8 +15,11 @@ from django.urls import path
 from game import consumers
 from django.urls import re_path
 from django.core.asgi import get_asgi_application
-
+from channels.routing import get_default_application
+import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ludomission.settings')
+
+django.setup()
 django_asgi_app = get_asgi_application()
 
 
