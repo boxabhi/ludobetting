f49f07998cd528fadd21cf79d62de6dd9b3692a9
boxabhi <abhijeetg40@gmail.com>
2020-12-25 14:22:40 +0530

    -

--- ludomission/settings.py
+++ ludomission/settings.py
@@ -84,23 +84,23 @@ ASGI_APPLICATION = 'ludomission.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'ludomissiondb',
-#         'USER': 'ludomissionadmin',
-#         'PASSWORD': 'ludobet@456',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ludomissiondb',
+        'USER': 'ludomissionadmin',
+        'PASSWORD': 'ludobet@456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
